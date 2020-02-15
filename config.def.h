5fc624a41e140c2a68617411cf378a94566ca830
kanocyan <kanocyan@outlook.com>
2020-02-15 12:28:24 +0800

    Modify defaultfg defaultbg defaultcs defaultrcs to correct values

--- config.def.h
+++ config.def.h
@@ -192,9 +192,9 @@ ResourcePref resources[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 0;
-static unsigned int defaultcs = 256;
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+static unsigned int defaultcs = 258;
 static unsigned int defaultrcs = 257;
 
 /*
